a3c7de109fb158c81f182d08946339f07935a83b
Mikeal Rogers <mikeal.rogers@gmail.com>
2009-10-25 23:35:54 -0700

    Removing line from docs.

--- couchdb_wsgi/__init__.py
+++ couchdb_wsgi/__init__.py
@@ -9,29 +9,7 @@ except:
     # Prior to 2.6 requires simplejson
     import simplejson as json
     
-import urlencoding    
-
-"""
-{
-    'body': 'undefined',
-    'cookie': {
-        '__utma': '96992031.3087658685658095000.1224404084.1226129950.1226169567.5',
-        '__utmz': '96992031.1224404084.1.1.utmcsr'
-    },
-    'form': {},
-    'info': {
-        'compact_running': False,
-        'db_name': 'couchbox',
-        'disk_size': 50559251,
-        'doc_count': 9706,
-        'doc_del_count': 0,
-        'purge_seq': 0,
-        'update_seq': 9706},
-    'path': [],
-    'query': {},
-    'verb': 'GET'
-}
-"""
+import urlencoding
 
 class CouchDBWSGIHandler(object):
     def __init__(self, request):
